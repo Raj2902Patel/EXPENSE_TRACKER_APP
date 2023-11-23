7ed89be6b1c9641a4d3788ecaa28f638048dbd6e
Raj Patel <rajpatel.imscit20@gmail.com>
2023-11-23 21:07:28 +0530

    Updated

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -5,11 +5,28 @@ import 'package:expense_tracker/widgets/expenses.dart';
 var kColorScheme =
     ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
 
-
+var kdarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 5, 99, 125),
+);
 
 void main() {
   runApp(
     MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        useMaterial3: true,
+        colorScheme: kdarkColorScheme,
+        cardTheme: const CardTheme().copyWith(
+          color: kdarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kdarkColorScheme.primaryContainer,
+            foregroundColor: kdarkColorScheme.onPrimaryContainer,
+          ),
+        ),
+      ),
       theme: ThemeData().copyWith(
         useMaterial3: true,
         colorScheme: kColorScheme,
@@ -33,8 +50,8 @@ void main() {
                 fontSize: 16,
               ),
             ),
-        
       ),
+      themeMode: ThemeMode.system,
       home: const Expenses(),
     ),
   );
